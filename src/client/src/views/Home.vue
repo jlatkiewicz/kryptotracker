e8a98aa2a6ec6ba7c00a56f16b75a93f6cfe9223
porinesss <Punodyb1223>
2021-05-25 00:24:18 +0200

    v1 - robocze rzeczy

---
+++ src/client/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <b-row class="justify-content-center">
+    <div>
+      <b-card
+          align-h="center"
+          border-variant="dark"
+          bg-variant="light"
+          title="Let's start journey"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 70rem;"
+          class="mb-2 mx-auto"
+
+      >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </b-card-text>
+
+        <b-button to="/" variant="primary">Go home again</b-button>
+
+        <div class="container">
+          <h1>My First Bootstrap Page</h1>
+          <p>This is some text.</p>
+          <p><font-awesome-icon icon="bitcoin"/></p>
+        </div>
+      </b-card>
+
+<!--      <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">-->
+<!--        <p>For more information visit website</p>-->
+<!--        <b-button variant="primary" href="#">More Info</b-button>-->
+<!--      </b-jumbotron>-->
+    </div>
+  </b-row>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
---
+++ src/client/src/views/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <p></p>
+
+      </b-row>
+      <b-row class="justify-content-md-center mt-2">
+        <b-col col lg="1"></b-col>
+        <b-col cols="8" md="auto">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+                id="input-group-1"
+                label="Login:"
+                label-for="input-1"
+                description="Bla bla"
+            >
+              <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="email"
+                  placeholder="Enter email"
+                  required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+              <b-form-input
+                  id="input-2"
+                  v-model="form.name"
+                  placeholder="Enter password"
+                  required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary" class="mr-2">Login</b-button>
+            <b-button type="reset" variant="danger">Cancel</b-button>
+          </b-form>
+        </b-col>
+        <b-col col lg="1"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{text: 'Select One', value: null}, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
