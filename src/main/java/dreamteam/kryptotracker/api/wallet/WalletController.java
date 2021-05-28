909eefdd2f51c3a882647473cc9c030614a0ef4d
jlatkiewicz <jakub.latkiewicz@gmail.com>
2021-05-28 22:47:44 +0200

    RequestParam -> PathVariable

--- src/main/java/dreamteam/kryptotracker/api/wallet/WalletController.java
+++ src/main/java/dreamteam/kryptotracker/api/wallet/WalletController.java
@@ -2,10 +2,7 @@ package dreamteam.kryptotracker.api.wallet;
 
 import dreamteam.kryptotracker.domain.wallet.WalletService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -19,26 +16,26 @@ public class WalletController {
     }
 
     @GetMapping("/{userId}")
-    public Mono<WalletResponse> findBy(@RequestParam("userId") String userId) {
+    public Mono<WalletResponse> findBy(@PathVariable("userId") String userId) {
         return walletService.findBy(userId)
                 .map(wallet -> WalletResponse.from(wallet, userId))
                 .switchIfEmpty(Mono.error(new UsernameNotFoundException(userId)));
     }
 
     @PostMapping("/add/{userId}/{amount}")
-    public Mono<WalletResponse> addBitcoin(@RequestParam("userId") String userId, @RequestParam("amount") String amount) {
+    public Mono<WalletResponse> addBitcoin(@PathVariable("userId") String userId, @RequestParam("amount") String amount) {
         return walletService.addBitcoin(userId, new BigDecimal(amount))
                 .map(wallet -> WalletResponse.from(wallet, userId));
     }
 
     @PostMapping("/subtract/{userId}/{amount}")
-    public Mono<WalletResponse> subtractBitcoin(@RequestParam("userId") String userId, @RequestParam("amount") String amount) {
+    public Mono<WalletResponse> subtractBitcoin(@PathVariable("userId") String userId, @RequestParam("amount") String amount) {
         return walletService.subtractBitcoin(userId, new BigDecimal(amount))
                 .map(wallet -> WalletResponse.from(wallet, userId));
     }
 
     @PostMapping("/set/{userId}/{amount}")
-    public Mono<WalletResponse> setBitcoin(@RequestParam("userId") String userId, @RequestParam("amount") String amount) {
+    public Mono<WalletResponse> setBitcoin(@PathVariable("userId") String userId, @RequestParam("amount") String amount) {
         return walletService.setBitcoin(userId, new BigDecimal(amount))
                 .map(wallet -> WalletResponse.from(wallet, userId));
     }
